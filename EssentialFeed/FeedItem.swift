c889359a268fa93047585f9bdfd949833eb09414
Matthew Houston <matthew.houston7@icloud.com>
2022-09-28 19:45:42 -0700

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Matthew Houston on 9/28/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+    
+}
