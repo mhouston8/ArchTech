401b401bbbc616122dedfc69051b7026946c5f06
Matthew Houston <matthew.houston7@icloud.com>
2022-10-05 23:57:00 -0700

    Create the FeedLoader protocol boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Matthew Houston on 10/5/22.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+//This is the protocol
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
