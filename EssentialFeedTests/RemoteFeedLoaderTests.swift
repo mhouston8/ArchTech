a656dec07f75c1ca5384f55ee6e2113a507cf1fb
Matthew Houston <matthew.houston7@icloud.com>
2022-10-29 09:36:36 -0700

    Give a URL instead of hardcoding it in the RemoteFeedLoader. Dependency Injection. (Constructor Injection)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,13 +10,15 @@ import XCTest
 class RemoteFeedLoader {
     
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "www.google.com")!)
+        client.get(from: url)
     }
 }
 
@@ -39,23 +41,28 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url,client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
-        
-        client.requestedURL = URL(string: "www.google.com")
+        let sut = RemoteFeedLoader(url: url, client: client)
+    
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
+
     
+    
+    
+
     
     
     
